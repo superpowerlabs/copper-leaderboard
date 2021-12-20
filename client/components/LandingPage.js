d22dbf44f60e7da78e176a337147ad9807bc1d4c
Gabriel Vazquez <gavazcal@gmail.com>
2021-12-20 15:09:37 -0400

    progress bar, buy syn button, missing styling

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -1,6 +1,10 @@
 // eslint-disable-next-line no-undef
 import Loading from './lib/Loading'
 import NFTs from './NFTs'
+import Progressbar from './ProgressBar'
+import Button from './BuySynbtn'
+
+const progress_now = 25
 
 const {Container} = ReactBootstrap
 // eslint-disable-next-line no-undef
@@ -37,6 +41,17 @@ export default class LandingPage extends React.Component {
           {//<div className={'centered imgLogo'}><img src={'/images/syn-city-large.png'} alt={'Syn City logo'}/></div>
           }
             <NFTs/>
+            <div className="App">
+              <p>
+                <div classname="progressBarComplete">
+              <h4 className="progressBarAddress">You:address placeholder</h4>
+            <div className="progressBar">
+                <Progressbar bgcolor="yellow" progress={progress_now} height={40} />
+                <Button classname="buySYNbtn" text="BUY $SYN"/>
+                </div>
+            </div>
+            </p>
+            </div>
 
           <br style={{clear: 'both'}} />
           <div className={'haveFun'}><h1 className={'centered'}>Have fun!</h1></div>
