afc011051ef40708663eaf84795586f7c399cd84
jerrybassat7 <jerry.bassat@upr.edu>
2021-12-20 02:22:09 -0400

    Working on 3 nfts display

--- client/components/NFTs.js
+++ client/components/NFTs.js
@@ -9,9 +9,20 @@ export default class NFTs extends React.Component {
   render() {
     return (<div>
         <Container className={'topContainer'}>
-        <div className={'yellowLogo'}><img src={'/images/yellowLogo.png'} alt={'logo'}/></div>
+        <div className={'topsynnersLogo'}><img src={'/images/topSinners.png'} alt={'logo'}/></div>
           <div className={'home-section centered'}>
-            <h1 className={'notTooLarge'}>CACAPOPO</h1>
+            <h1 className={'landingpageText'}>We believe in rewarding our community, especially the early supporters, which is why we have decided to reward our early supporters with exclusive "Original SYNNER" NFTs to the top contributors during our launch auction!</h1>
+          </div>
+          <div>
+          <div className={'nftLogo'}>
+              <div>
+              <img src={'/images/NFT01.png'} alt={'logo'}/>
+              <h1 className={'nftText'}>Top 50 reward:</h1>
+
+              </div>
+              <img src={'/images/NFT02.png'} alt={'logo'}/>
+              <img src={'/images/NFT03.png'} alt={'logo'}/>
+              </div>
           </div>
         </Container>
       </div>
