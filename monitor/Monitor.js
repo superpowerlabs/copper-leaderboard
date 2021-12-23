913d1749c1bf489cc6c2dfca6e331b9e41da74ca
Francesco Sullo <sullof@gmail.com>
2021-12-23 12:16:53 -0800

    Setting a monitor

---
+++ monitor/Monitor.js
@@ -0,0 +1,34 @@
+
+
+const dbManager = require('../server/lib/DbManager')
+
+
+async function sleep(millis) {
+  // eslint-disable-next-line no-undef
+  return new Promise((resolve) => setTimeout(resolve, millis));
+}
+
+
+class Monitor {
+
+  async getEvents() {
+    let events;
+
+
+    await sleep(1000);
+    return events;
+  }
+
+
+  async start() {
+
+    let i = 1
+    for (;;) {
+      console.log(i++)
+      let newEvents = await this.getEvents()
+
+    }
+  }
+}
+
+module.exports = new Monitor()
