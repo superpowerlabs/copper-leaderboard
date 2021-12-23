a1e04fd108d136658f200d0c74eb31837523cf2d
Francesco Sullo <sullof@gmail.com>
2021-12-23 10:56:41 -0800

    Remove sign.js

--- sign.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const { validator, validator2 } = require("./env.json");
-const ethers = require("ethers");
-const [, , hash, nodeEnv] = process.argv;
-const signingKey = new ethers.utils.SigningKey(
-  nodeEnv === "development" ? validator2 : validator
-);
-const signedDigest = signingKey.signDigest(hash);
-console.info(ethers.utils.joinSignature(signedDigest));
