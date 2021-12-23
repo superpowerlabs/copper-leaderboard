bc7b5fdb40ac55c1f193011cb3ff3161158220f8
jerrybassat7 <jerry.bassat@upr.edu>
2021-12-23 00:43:46 -0400

    FIxed some progress bar styling, Added Querying for blockchain trnasactions for ERC20 blockchain

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -1,66 +1,76 @@
 // eslint-disable-next-line no-undef
-import Loading from './lib/Loading'
-import NFTs from './NFTs'
-import Leaderboard from './Leaderboard'
-import Progressbar from './ProgressBar'
-import Button from './BuySynbtn'
+import Loading from "./lib/Loading";
+import NFTs from "./NFTs";
+import Leaderboard from "./Leaderboard";
+import Progressbar from "./ProgressBar";
+import Button from "./BuySynbtn";
 
-
-const {Container} = ReactBootstrap
+const { Container } = ReactBootstrap;
 // eslint-disable-next-line no-undef
-const {Redirect} = ReactRouterDOM
-const progress_now = 25
+const { Redirect } = ReactRouterDOM;
+const progress_now = 25;
 // eslint-disable-next-line no-undef
 export default class LandingPage extends React.Component {
-
-
   componentDidMount() {
     // window.location = 'https://syn.city'
   }
 
   links() {
-    const baseUri = `https://syn-city-nfts.s3.us-east-2.amazonaws.com/arg2-images/`
-    const images = 'step1-hqimage.png,step2-royimage.png,step3-devanshimage.png,step4-zhiminimage.png,step5-sullofimage.png'.split(',')
-    const rows = []
+    const baseUri = `https://syn-city-nfts.s3.us-east-2.amazonaws.com/arg2-images/`;
+    const images =
+      "step1-hqimage.png,step2-royimage.png,step3-devanshimage.png,step4-zhiminimage.png,step5-sullofimage.png".split(
+        ","
+      );
+    const rows = [];
     for (let i = 0; i < 5; i++) {
-      rows.push(<div>
-        <br style={{clear: 'both'}} />
-        <div key={'key' + Math.random()} className={'thumbs'}>
-          <img src={baseUri + images[i]}/>
+      rows.push(
+        <div>
+          <br style={{ clear: "both" }} />
+          <div key={"key" + Math.random()} className={"thumbs"}>
+            <img src={baseUri + images[i]} />
+          </div>
+          <div className={"thumbsLabel"}>Image #{i + 1}</div>
         </div>
-        <div className={'thumbsLabel'}>Image #{i + 1}</div>
-      </div>)
+      );
     }
-    return rows
+    return rows;
   }
 
   render() {
-
-
-    return (<div>
-        <Container className={'topContainer'}>
-          <div className={'yellowLogo'}><img src={'/images/yellowLogo.png'} alt={'logo'}/></div>
-          {//<div className={'centered imgLogo'}><img src={'/images/syn-city-large.png'} alt={'Syn City logo'}/></div>
+    return (
+      <div>
+        <Container className={"topContainer"}>
+          <div className={"yellowLogo"}>
+            <img src={"/images/yellowLogo.png"} alt={"logo"} />
+          </div>
+          {
+            //<div className={'centered imgLogo'}><img src={'/images/syn-city-large.png'} alt={'Syn City logo'}/></div>
           }
-            <NFTs/>
-            <div className="App">
-              <p>
-                <div classname="progressBarComplete">
-              <h4 className="progressBarAddress">You:address placeholder</h4>
-            <div className="progressBar">
-                <Progressbar bgcolor="yellow" progress={progress_now} height={40} />
-                <Button classname="buySYNbtn" text="BUY $SYN"/>
+          <NFTs />
+          <div className="App">
+            <p>
+              <div classname="progressBarComplete">
+                <h4 className="progressBarAddress">You:address placeholder</h4>
+                <div className="progressBar">
+                  <div className="progressBar2">
+                    <Progressbar
+                      bgcolor="yellow"
+                      progress={progress_now}
+                      height={40}
+                    />
+                  </div>
+                  <div className="buySYNbtn2">
+                    <Button classname="buySYNbtn" text="BUY $SYN" />
+                  </div>
                 </div>
-            </div>
+              </div>
             </p>
-            </div>
+          </div>
 
-          <br style={{clear: 'both'}} />
-          <div className={'haveFun'}><h1 className={'centered'}>Have fun!</h1></div>
-
-          <Leaderboard />
+          <br style={{ clear: "both" }} />
         </Container>
+        <Leaderboard />
       </div>
-    )
+    );
   }
 }
--- server/app.js
+++ server/app.js
@@ -1,125 +1,126 @@
-const express = require('express')
-const fs = require('fs-extra')
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const metaApi = require('./routes/metaApi')
-const authApi = require('./routes/authApi')
-const Logger = require('./lib/Logger')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const rateLimit = require('express-rate-limit')
+const express = require("express");
+const fs = require("fs-extra");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const metaApi = require("./routes/metaApi");
+const authApi = require("./routes/authApi");
+const Logger = require("./lib/Logger");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const queryService = require("./blockchainQueryService");
 
-process.on('uncaughtException', function (error) {
-
-  Logger.error(error.message)
-  Logger.error(error.stack)
+process.on("uncaughtException", function (error) {
+  Logger.error(error.message);
+  Logger.error(error.stack);
 
   // if(!error.isOperational)
   //   process.exit(1)
-})
+});
 
-let indexText
+let indexText;
 
 function getIndex() {
   if (!indexText) {
-    indexText = fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf-8')
+    indexText = fs.readFileSync(
+      path.resolve(__dirname, "../public/index.html"),
+      "utf-8"
+    );
   }
-  return indexText
+  return indexText;
 }
 
-const app = express()
+const app = express();
 
 const limiter = rateLimit({
   windowMs: 10 * 1000,
-  max: 60
-})
+  max: 60,
+});
 
-app.use(limiter)
+app.use(limiter);
 
-app.use(cors())
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
-app.use('/auth', authApi)
+app.use("/auth", authApi);
 
 app.use(async (req, res, next) => {
-  if (req.hostname === 'arg.syn.city') {
-    res.redirect('https://nft.syn.city')
+  if (req.hostname === "arg.syn.city") {
+    res.redirect("https://nft.syn.city");
   } else {
-    next()
+    next();
   }
-})
+});
 
-app.use('/meta/:symbol/:id', async (req, res) => {
-  res.json(metaApi.getMetadata(res, req))
-})
+app.use("/meta/:symbol/:id", async (req, res) => {
+  res.json(metaApi.getMetadata(res, req));
+});
 
-app.use('/meta/:symbol', async (req, res) => {
-  res.json(metaApi.getContractMetadata(res, req))
-})
+app.use("/meta/:symbol", async (req, res) => {
+  res.json(metaApi.getContractMetadata(res, req));
+});
 
-app.use('/index.html', function (req, res) {
-  res.redirect('/')
-})
+app.use("/index.html", function (req, res) {
+  res.redirect("/");
+});
 
-app.use('/healthcheck', function (req, res) {
-  res.send('ok')
-})
+app.use("/healthcheck", function (req, res) {
+  res.send("ok");
+});
 
-
-app.use('/:anything', function (req, res, next) {
-  let v = req.params.anything
+app.use("/:anything", function (req, res, next) {
+  let v = req.params.anything;
   switch (v) {
-    case 'favicon.png':
-    case 'styles':
-    case 'images':
-    case 'nft':
-    case 'bundle':
-      next()
-      break
+    case "favicon.png":
+    case "styles":
+    case "images":
+    case "nft":
+    case "bundle":
+      next();
+      break;
     default:
-      res.send(getIndex())
+      res.send(getIndex());
   }
-})
+});
 
-app.use(express.static(path.resolve(__dirname, '../public')))
-
+app.use(express.static(path.resolve(__dirname, "../public")));
+queryService.getContract();
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
 
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
   app.use((err, req, res, next) => {
     res.status(err.status || 500).json({
-      title: 'Error',
+      title: "Error",
       message: err.message,
-      error: err
-    })
-  })
+      error: err,
+    });
+  });
 }
 
 // error handler
 app.use(function (err, req, res, next) {
-
   if (err.status !== 404) {
-    console.debug(err)
-    console.debug(err.status)
-    console.debug(err.message)
+    console.debug(err);
+    console.debug(err.status);
+    console.debug(err.message);
   }
 
   // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  res.json({error: 'Error'})
-})
+  res.status(err.status || 500);
+  res.json({ error: "Error" });
+});
 
 // app.closeDb = () => {
 //   // if (db.isOpen()) {
@@ -130,6 +131,4 @@ app.use(function (err, req, res, next) {
 //   return app
 // }
 
-
-module.exports = app
-
+module.exports = app;
