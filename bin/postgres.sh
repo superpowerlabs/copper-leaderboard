b5e875ada8cab0d032f68058c34c3ee6c397cdb6
Francesco Sullo <sullof@gmail.com>
2021-12-28 12:05:05 -0800

    Remove pgdata

--- bin/postgres.sh
+++ bin/postgres.sh
@@ -4,14 +4,12 @@
 # POSTGRES_USER=postgres
 # POSTGRES_PASSWORD=iKANTknowIT
 # POSTGRES_DB=leaderboard
-# PGDATA=`pwd`/pgdata
 
 source .env && docker run -d \
     --name leaderboard-postgres \
     --restart unless-stopped \
     -p 5432:5432 \
     -v `pwd`/data:/var/lib/postgresql/data/pgdata \
-    -e PGDATA=/var/lib/postgresql/data/pgdata \
     -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
     -e POSTGRES_USER=$POSTGRES_USER \
     -e POSTGRES_DB=$POSTGRES_DB \
