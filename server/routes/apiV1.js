48b0a960a5b4f253cbeeb08332e2f62021aaca28
roquesrolando <roquesrolando@hotmail.com>
2021-12-30 13:54:26 -0400

    resolved conflicts

--- server/routes/apiV1.js
+++ server/routes/apiV1.js
@@ -10,4 +10,15 @@ router.get("/investments", async (req, res) => {
   });
 });
 
+router.post("/investment", async (req, res) => {
+  // const ammount = req.body;
+  // const wallet = req.body;
+  // const hash = req.txhash;
+  const newinvestment = await dbManager.newInvestment(5, "wallet", "tx_hash");
+  res.json({
+    success: true,
+    newinvestment,
+  });
+});
+
 module.exports = router;
