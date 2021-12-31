84f817d7f9a57e2f8c8f877e5ecd347b95b777a7
Francesco Sullo <sullof@gmail.com>
2021-12-31 09:58:35 -0800

    Silencing warnings

--- monitor/blockchainQueryService.js
+++ monitor/blockchainQueryService.js
@@ -7,50 +7,44 @@ const START_BLOCK = 7700000;
 const END_BLOCK = 7700100;
 
 const queryService = {
-    async getEvents() {
-        const provider = new Ethers.providers.InfuraProvider("mainnet", {
-            projectId: "a5d8ae5cf48e49269d71a5cf25289c0d",
-        });
-        const contract = new Ethers.Contract(
-            CONTRACT_ADDRESS,
-            ERC20abi,
-            provider
-        );
-        const oldevents = await contract.queryFilter(
-            "Transfer",
-            START_BLOCK,
-            END_BLOCK
-        );
-        for (let i = 0; i < oldevents.length; i++) {
-            console.log(oldevents[i]);
-            console.log(oldevents[i].transactionHash);
-            console.log(oldevents[i].args.from);
-            console.log(Ethers.utils.formatEther(oldevents[i].args.value));
-            const etherValue = Ethers.utils.formatEther(
-                oldevents[i].args.value
-            );
-            const hash = oldevents[i].transactionHash;
-            const wallet = oldevents[i].args.from;
-            const newinvestment = await dbManager.newInvestment(
-                etherValue,
-                wallet,
-                hash
-            );
-            console.log(newinvestment);
-        }
+  async getEvents() {
+    const provider = new Ethers.providers.InfuraProvider("mainnet", {
+      projectId: "a5d8ae5cf48e49269d71a5cf25289c0d",
+    });
+    const contract = new Ethers.Contract(CONTRACT_ADDRESS, ERC20abi, provider);
+    const oldevents = await contract.queryFilter(
+      "Transfer",
+      START_BLOCK,
+      END_BLOCK
+    );
+    for (let i = 0; i < oldevents.length; i++) {
+      console.log(oldevents[i]);
+      console.log(oldevents[i].transactionHash);
+      console.log(oldevents[i].args.from);
+      console.log(Ethers.utils.formatEther(oldevents[i].args.value));
+      const etherValue = Ethers.utils.formatEther(oldevents[i].args.value);
+      const hash = oldevents[i].transactionHash;
+      const wallet = oldevents[i].args.from;
+      const newinvestment = await dbManager.newInvestment(
+        etherValue,
+        wallet,
+        hash
+      );
+      console.log(newinvestment);
+    }
 
-        contract.on("Transfer", async (from, to, value, event) => {
-            console.log(event);
-            const etherValue = Ethers.utils.formatEther(event.args.value);
-            const hash = event.transactionHash;
-            const wallet = event.args.from;
-            const newinvestment = await dbManager.newInvestment(
-                etherValue,
-                wallet,
-                hash
-            );
-            console.log(newinvestment);
-        });
-    },
+    contract.on("Transfer", async (from, to, value, event) => {
+      console.log(event);
+      const etherValue = Ethers.utils.formatEther(event.args.value);
+      const hash = event.transactionHash;
+      const wallet = event.args.from;
+      const newinvestment = await dbManager.newInvestment(
+        etherValue,
+        wallet,
+        hash
+      );
+      console.log(newinvestment);
+    });
+  },
 };
 module.exports = queryService;
