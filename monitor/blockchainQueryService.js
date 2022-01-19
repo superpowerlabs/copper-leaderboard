11bc4a4d0db903059fff4b927d59555205263403
roquesrolando <roquesrolando@hotmail.com>
2022-01-18 23:10:06 -0400

    title

--- monitor/blockchainQueryService.js
+++ monitor/blockchainQueryService.js
@@ -42,10 +42,9 @@ const queryService = {
           console.log(hash);
           console.log(wallet);
           console.log(event[i].topics);
-          
+
           //Make it so initial contract founding doesnt show in db
-          if ( ! (wallet === "0x3Aa9e065d90DB8ECDc641F2AEB3268a3de33D2ca"))
-          {
+          if (!(wallet === "0x3Aa9e065d90DB8ECDc641F2AEB3268a3de33D2ca")) {
             const newinvestment = await dbManager.newInvestment(
               syn,
               wallet,
