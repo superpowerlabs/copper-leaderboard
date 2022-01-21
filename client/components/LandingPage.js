ece58b8856aa8a3ae0419749c56438498d7fe126
roquesrolando <roquesrolando@hotmail.com>
2022-01-21 13:48:39 -0400

    quick fix to deployment

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -78,7 +78,8 @@ export default class LandingPage extends Base {
                     link={"https://bit.ly/3Kujw3k"}
                   />
                   . Ranking is based on net SYNR holding in your Ethereum wallet
-                  address. A snapshot at the end of the auction will decide the winners.
+                  address. A snapshot at the end of the auction will decide the
+                  winners.
                 </div>
               </div>
 
