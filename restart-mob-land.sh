8d608e8918059c9de82ed07cdee9d9422a1def6b
Francesco Sullo <sullof@gmail.com>
2022-02-13 16:32:01 -0800

    Fix restart

--- restart-mob-land.sh
+++ restart-mob-land.sh
@@ -1,3 +1,4 @@
 #!/bin/env bash
 
-git pull && pnpm i && pnpm build-mob-land && pm2 delete leaderboard && pm2 restart index --name leaderboard && pm2 save
+git pull && pnpm i && pnpm build-mob-land &&
+    pm2 delete leaderboard && ./start.sh
--- restart-syn-city.sh
+++ restart-syn-city.sh
@@ -1,3 +1,5 @@
 #!/bin/env bash
 
-git pull && pnpm i && pnpm build-syn-city && pm2 delete leaderboard && pm2 restart index --name leaderboard && pm2 save
+git pull && pnpm i && pnpm build-syn-city &&
+    pm2 delete leaderboard && ./start.sh
+
