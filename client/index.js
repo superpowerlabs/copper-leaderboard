f72777e8953253438dfa4b88bdcfb861503c6360
Yacin Bahi <yacin@red64.io>
2022-09-08 11:51:08 +0200

    removing test line/comment

--- client/index.js
+++ client/index.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 const App = require("./components/App");
 
-// ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
 ReactDOM.render(<App />, document.getElementById("root"));
